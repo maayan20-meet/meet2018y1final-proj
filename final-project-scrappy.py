972b55c11e957423059be162272ee2ce4d157168
maayan20-meet <maayan20@meet.mit.edu>
2018-08-15 15:32:34 -0400

    Stopping here

--- final-project-scrappy.py
+++ final-project-scrappy.py
@@ -1,7 +1,10 @@
 
 import turtle
 import time
+import random
 
+
+count_riddle = 1
 roomT = turtle.clone()
 room_count = 0
 u_door = None
@@ -26,7 +29,115 @@ def timer():
     turtle.ontimer(timer, 1000)
 
 
+#door1 = turtle.clone()
+#door2 = turtle.clone()
+#door3 = turtle.clone()
 
+
+screen.addshape('door1.gif')
+screen.addshape('door2.gif')
+screen.addshape('door3.gif')
+screen.addshape('door4.gif')
+screen.addshape('door5.gif')
+screen.addshape('door6.gif')
+screen.addshape('door7.gif')
+
+door_shapes = ['door1.gif', 'door2.gif', 'door3.gif', 'door4.gif', 'door5.gif', 'door6.gif', 'door7.gif']
+
+
+door1 = turtle.clone()
+door2 = turtle.clone()
+door3 = turtle.clone()
+
+riddleT1 = turtle.clone()
+riddleT2 = turtle.clone()
+riddleT3 = turtle.clone()
+riddleT4 = turtle.clone()
+riddleHL = turtle.clone()
+
+riddleT1.speed(0)
+riddleT2.speed(0)
+riddleT3.speed(0)
+riddleT4.speed(0)
+riddleHL.speed(0)
+
+riddleT1.penup()
+riddleT2.penup()
+riddleT3.penup()
+riddleT4.penup()
+riddleHL.penup()
+
+
+
+def ask_riddle():
+
+    door1.hideturtle()
+    door2.hideturtle()
+    door3.hideturtle()
+
+    riddle_list=["How many trees are cut down per year for toilet paper?","how many sea creaturs die due to plastic polution each year?","how old are the world's oldest trees?","how many plastic water buttles used in year??"]
+    global count_riddle
+
+    riddleHL.goto(0,250)
+    riddleT1.goto(-100, 100)
+    riddleT2.goto(100, 100)
+    riddleT3.goto(100, -100)
+    riddleT4.goto(-100, -100)
+
+    riddleHL.write(riddle_list[count_riddle], move = True, align = 'center', font=("David",20,"normal"))
+    riddleT1.write('2,000', move = True, align = 'center', font=("David",30,"normal"))
+    riddleT2.write('12,000', move = True, align = 'center', font=("David",30,"normal"))
+    riddleT3.write('27,000', move = True, align = 'center', font=("David",30,"normal"))
+    riddleT4.write('120,000', move = True, align = 'center', font=("David",30,"normal"))
+
+    riddleT1.goto(-120, 100)
+    riddleT2.goto(80, 100)
+    riddleT3.goto(80, -100)
+    riddleT4.goto(-120, -100)
+
+    riddleT1.onclick(riddle_answer1)
+    riddleT2.onclick(riddle_answer2)
+    riddleT3.onclick(riddle_answer3)
+    riddleT4.onclick(riddle_answer4)
+
+    s
+
+    if count_riddle == 2:
+        riddleHL.goto(0,250)
+        riddleT1.goto(-100, 100)
+        riddleT2.goto(100, 100)
+        riddleT3.goto(100, -100)
+        riddleT4.goto(-100, -100)
+
+        riddleHL.write('how many sea creaturs die due to plastic polution each year?', move = True, align = 'center', font=("David",20,"normal"))
+        riddleT1.write('100,000', move = True, align = 'center', font=("David",30,"normal"))
+        riddleT2.write('500,000', move = True, align = 'center', font=("David",30,"normal"))
+        riddleT3.write('750,000', move = True, align = 'center', font=("David",30,"normal"))
+        riddleT4.write('1,000,000', move = True, align = 'center', font=("David",30,"normal"))
+
+        riddleT1.goto(-120, 100)
+        riddleT2.goto(80, 100)
+        riddleT3.goto(80, -100)
+        riddleT4.goto(-120, -100)
+
+        riddleT1.onclick(riddle_answer1)
+        riddleT2.onclick(riddle_answer2)
+        riddleT3.onclick(riddle_answer3)
+        riddleT4.onclick(riddle_answer4)
+    
+
+        
+         
+    '''
+    
+
+    if count_riddle = 3:
+
+    if count_riddle = 4:
+    '''
+
+
+'''
 
 def riddle1():
     
@@ -36,7 +147,8 @@ def riddle1():
     print(" 1 = 2000, 2 = 12000, 3 = 27000, 4 = 120000")
     user_answer = int(input("What number would you like to choose?"))
     if user_answer == 3:
-        print('HINT: in room 1 the best door is 1+1-1+1-1')
+        print('HINT: in room 1 the bes
+        t door is 1+1-1+1-1')
     else:
         print('wrong! better luck next time')
 
@@ -47,7 +159,7 @@ def riddle2():
     if user_answer == 4:
        print("HINT:in room number 2 the third door is the worst")
     else:
-        print('wrong! better luck next time')
+print('wrong! better luck next time')
 
 def riddle3():
     print("how old are the world's oldest trees?")
@@ -67,11 +179,11 @@ def riddle4():
     else:
         print('wrong! better luck next time')
         ###############################################################
-
+'''
 def chose_door():
     global u_door
     global current_room
-    global room_count
+    
 
     
     
@@ -84,7 +196,7 @@ def chose_door():
 
 
     
-    draw_doors()
+    ask_riddle()
     
 
     #(current_room)(u_door)
@@ -92,6 +204,7 @@ def chose_door():
 def room_0(u_door):
     #riddle2()
     if u_door == 1:
+        global current_room, room_count
         print('You made it!')
         current_room = room_1
         room_count += 1
@@ -109,6 +222,7 @@ def room_0(u_door):
 
 def room_1(u_door):
     #riddle3()
+    global current_room, room_count
     if u_door == 1:
         print('oh no its a trap')
         current_room = room_0
@@ -127,6 +241,7 @@ def room_1(u_door):
 
 def room_2(u_door):
     #riddle4()
+    global current_room, room_count
     if u_door == 1:
         print('oh no its a trap')
         current_room = room_0
@@ -136,12 +251,14 @@ def room_2(u_door):
         print('You made it!')
         current_room = room_3
         room_count += 1
-        chose_door
+        chose_door()
     elif u_door == 3:
         print('oh no its a trap')
         current_room = room_0
+        room_count = 0
         chose_door()
 def room_3(u_door):
+    global current_room, room_count
     if u_door == 1:
         print('oh no its a trap')
         current_room = room_0
@@ -160,27 +277,35 @@ def room_3(u_door):
 
 def draw_doors():
     global u_door, screen
-    #global current_room
+    global current_room
     
-    
-    door1 = turtle.clone()
-    door2 = turtle.clone()
-    door3 = turtle.clone()
+    riddleT1.clear()
+    riddleT2.clear()
+    riddleT3.clear()
+    riddleT4.clear()
+    riddleHL.clear()
+       
     
     door1.penup()
     door2.penup()
     door3.penup()
 
+    
+
     door1.speed(0)
     door2.speed(0)
     door3.speed(0)
     
-    screen.addshape('door1.gif')
-    door1.shape('door1.gif')
-    screen.addshape('door2.gif')
-    door2.shape('door2.gif')
-    screen.addshape('door3.gif')
-    door3.shape('door3.gif')
+    
+    door1.shape(random.choice(door_shapes))
+    
+    door2.shape(random.choice(door_shapes))
+    
+    door3.shape(random.choice(door_shapes))
+
+    door1.showturtle()
+    door2.showturtle()
+    door3.showturtle()
 
     door1.goto(-200,0)
     door2.goto(0, 0)
@@ -205,7 +330,8 @@ def enter_door():
 
     
      
-    #print()
+
+    print()
 
 
 def fun_door1(x, y):
@@ -224,6 +350,23 @@ def fun_door3(x, y):
     u_door = 3
     door_clicked = True
     enter_door()
+
+
+def riddle_answer1(x,y):
+    print('oh no wrong answer')
+    draw_doors()
+
+def riddle_answer2(x,y):
+    print('oh no wrong answer')
+    draw_doors()
+
+def riddle_answer3(x,y):
+    print('HINT: in room number 1 the right door is x/2 = 4')
+    draw_doors()
+
+def riddle_answer4(x,y):
+    print('oh no wrong answer')
+    draw_doors()
              
 current_room = room_0
 timer()
